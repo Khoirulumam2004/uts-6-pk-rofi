7c7181dabb38688640822510245d4018e4173166
unknown <khirulumam2004@example.com>
2024-07-06 12:45:16 +0100

    tugas uas

--- api/serializers.py
+++ api/serializers.py
@@ -5,14 +5,14 @@ from .models import Teknik, Computer, Elit
 class TeknikSerializers(serializers.ModelSerializer):
     class Meta:
         model = Teknik
-        fields = ["id", "title", "content", "published_date"]
+        fields = ["id", "haid", "tanggal awal haid", "hal waktu haid"]
 
 class ComputerSerializers(serializers.ModelSerializer):
     class Meta:
         model = Computer
-        fields = ["id", "title", "content", "published_date"]
+        fields = ["id", "haid", "tanggal awal haid", "hal waktu haid"]
 
 class ElitSerializers(serializers.ModelSerializer):
     class Meta:
         model = Elit
-        fields = ["id", "title", "content", "published_date"]
+        fields = ["id", "haid", "tanggal awal haid", "hal waktu haid"]
