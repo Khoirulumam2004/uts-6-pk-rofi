d6036767ddfc951b51c86a0138f4ad5d52308a4b
unknown <khirulumam2004@example.com>
2024-07-07 11:12:20 +0100

    tugas uas

--- api/serializers.py
+++ api/serializers.py
@@ -6,13 +6,20 @@ class TeknikSerializers(serializers.ModelSerializer):
     class Meta:
         model = Teknik
         fields = ["id", "haid", "tanggal awal haid", "hal waktu haid"]
-
+            class meta:
+                model = teknik 
+                fields = ["darah yang keluar diwaktu sehat"]
 class ComputerSerializers(serializers.ModelSerializer):
     class Meta:
         model = Computer
         fields = ["id", "haid", "tanggal awal haid", "hal waktu haid"]
-
+            class meta:
+            model = teknik 
+            fields = ["tentukan tanggal"]
 class ElitSerializers(serializers.ModelSerializer):
     class Meta:
         model = Elit
         fields = ["id", "haid", "tanggal awal haid", "hal waktu haid"]
+            class meta:
+                model = elit 
+                fields = ["berbuat baik, perbanyak sedekah, perbanyak dzikir"]
